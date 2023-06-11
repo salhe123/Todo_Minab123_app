04cd3bc4d1029b4930ad61b6c6aaaf8d18e1445c
salhe <ibnuseid27@gmail.com>
2023-06-11 22:10:19 +0300

    app.vue commited

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <UserList />
+    <Addauser />
   </div>
 </template>
 
 <script >
+import UserList from './components/UserList.vue';
 import UserList from './components/UserList.vue';
 export default
   {
