4d49ac358b2c94d7f6ffe47e768fe6eaddc3b868
salhe <ibnuseid27@gmail.com>
2023-06-15 22:37:06 +0300

    night

--- src/components/showtask.vue
+++ src/components/showtask.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <h1>{{ user.fname }}'s Todos</h1>
-    <ul>
-      <li v-for="todo in user.users_todos" :key="todo.id">
-        <h2>{{ todo.title }}</h2>
-        <p>{{ todo.description }}</p>
+  <div class="bg-neutral-400 text-center">
+    <h1 class="bg-neutral-400 text-left pt-10 ml-28 font-bold h-14 text-3xl">{{ user && user.fname }}'s Todos:</h1>
+    <ul v-if="user && user.users_todos">
+      <li class=" pt-20 pb-5" v-for="todo in user && user.users_todos" :key="todo.id">
+        <h2 class=" pt-10">Title:{{ todo.title }}</h2>
+        <p class=" pt-24 pb-5">{{ todo.description }}</p>
         <p>Completed: {{ todo.completed ? 'Yes' : 'No' }}</p>
       </li>
     </ul>
-    <button @click="handleBack">Back</button>
+    <button class="hover:bg-neutral-400 text-red-600 font-bold py-2 px-4 rounded ml-5 pt-3 " @click="handleBack">Back</button>
   </div>
 </template>
 
@@ -35,7 +35,7 @@ const { result } = useQuery(gql`
     }
   }
 `, {
-  userId: $route.params.id,
+  userId
 });
 
 const user = result.value?.users_by_pk;
