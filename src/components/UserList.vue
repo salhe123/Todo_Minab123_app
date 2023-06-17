04ac53ac8755cf977d2fa0abfc978f7bc7a0fb3c
salhe <ibnuseid27@gmail.com>
2023-06-17 09:14:24 +0300

    saturday

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -16,10 +16,12 @@
 </svg>
 
 </button></RouterLink>
-<router-link :to="{ name: 'ShowTask', params: { id: user.id },props:{fname:user.fname} }"><svg class="w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+<RouterLink :to="{ name: 'ShowTask', params: { id: user.id },props:{fname:user.fname} }">
+  <svg class="w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
 </svg>
-{{user.fname}}</router-link>
+{{user.fname}}
+</RouterLink>
         <button @click="handleDelete(user.id)"> <svg class="w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg></button>
